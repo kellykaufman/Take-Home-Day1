c6157b599dfb313b82cc1569b4c121b1056dfc31
Kelly Kaufman <kellykaufman12@gmail.com>
2022-04-11 17:09:14 -0700

    COmpletes Day 1 Hw

---
+++ hw1/src/utils.js
@@ -0,0 +1,23 @@
+// 8e16e290
+
+let API_KEY = "8e16e290";
+
+export const getMoviesByName = async (name) => {
+  const url = `https://www.omdbapi.com/?apikey=${API_KEY}&s=${name}`;
+  const response = await fetch(url);
+  const data = await response.json();
+  console.log(data);
+};
+
+getMoviesByName("Avengers");
+
+/////////////////////////////////////////////////
+
+export const getMovieDetailsById = async (id) => {
+  const url = `https://www.omdbapi.com/?apikey${API_KEY}&i=${id}`;
+  const response = await fetch(url);
+  const data = await response.json();
+  console.log(data);
+};
+
+getMovieDetailsById(1);
